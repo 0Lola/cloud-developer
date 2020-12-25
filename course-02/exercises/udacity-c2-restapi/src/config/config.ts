7080aaf85adb3263744d33174bf3a9e5d46c0cfc
Lola <zxa011023@gmail.com>
2020-12-25 12:02:32 +0800

    Update config.ts

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,19 +1,20 @@
 export const config = {
-  "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
-    "dialect": "postgres",
-    "aws_region": "us-east-2",
-    "aws_profile": "default",
-    "aws_media_bucket": "udagram-ruttner-dev"
-  },
-  "prod": {
-    "username": "",
-    "password": "",
-    "database": "udagram_prod",
-    "host": "",
-    "dialect": "postgres"
-  }
-}
+     "dev": {
+       "username": "udacitydbdev",
+       "password": "qwer1234",
+       "database": "udacitydbdev",
+       "host": "udacitydbdev.czig7i9wto5o.us-east-1.rds.amazonaws.com",
+       "dialect": "postgres",
+       "aws_region": "us-east-2",
+       "aws_profile": "default",
+       "aws_media_bucket": "udacity-filestore-dev"
+     },
+     "prod": {
+       "username": "",
+       "password": "",
+       "database": "udagram_prod",
+       "host": "",
+       "dialect": "postgres"
+     }
+   }
+   
