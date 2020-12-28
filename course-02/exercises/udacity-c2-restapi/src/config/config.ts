6ddb446cd7210e08ca294468c5ec1f5d4831bd22
Lola <zxa011023@gmail.com>
2020-12-28 17:58:45 +0800

    Update JWT

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,20 +1,22 @@
 export const config = {
-     "dev": {
-       "username": "udacitydbdev",
-       "password": "qwer1234",
-       "database": "udacitydbdev",
-       "host": "udacitydbdev.czig7i9wto5o.us-east-1.rds.amazonaws.com",
-       "dialect": "postgres",
-       "aws_region": "us-east-2",
-       "aws_profile": "default",
-       "aws_media_bucket": "udacity-filestore-dev"
-     },
-     "prod": {
-       "username": "",
-       "password": "",
-       "database": "udagram_prod",
-       "host": "",
-       "dialect": "postgres"
-     }
-   }
-   
+  "dev": {
+    "username": "udacitydbdev",
+    "password": "qwer1234",
+    "database": "udacitydbdev",
+    "host": "udacitydbdev.czig7i9wto5o.us-east-1.rds.amazonaws.com",
+    "dialect": "postgres",
+    "aws_region": "us-east-2",
+    "aws_profile": "default",
+    "aws_media_bucket": "udacity-filestore-dev"
+  },
+  "prod": {
+    "username": "",
+    "password": "",
+    "database": "udagram_prod",
+    "host": "",
+    "dialect": "postgres"
+  },
+  "jwt": {
+       "secret": "heyheyhey"
+  }
+}
